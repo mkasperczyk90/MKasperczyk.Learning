50225cc040dd4dde4eddee493a9032b2cbebe305
mkasperczyk <mkasperczyk@hotmail.com>
2022-09-06 08:19:10 +0200

    Learning - CleanCode: List Inheritance, Throw library

---
+++ DotNet/MKasperczyk.Learning.CleanCodeTips/MKasperczyk.Learning.CleanCodeTips/Generics/ListInheritance.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKasperczyk.Learning.CleanCodeTips.Generics;
+
+public class Examples
+{
+    public void Main()
+    {
+        // Cleaner:
+        CleanerLibrary library = new CleanerLibrary();
+        var result = library.Books.GetFavouriteBook();
+        Console.WriteLine(result);
+
+        // Worst: 
+        WorstLibrary secoundLibrary = new WorstLibrary();
+        var secoundResult = GetFavouriteBook(secoundLibrary);
+        Console.WriteLine(secoundResult);
+    }
+    public string GetFavouriteBook(WorstLibrary library)
+    {
+        return library.Books.First(x => x.StartsWith("J"));
+    }
+}
+
+public class Books : List<string>
+{
+    public string GetFavouriteBook()
+    {
+        return this.First(x => x.StartsWith("J"));
+    }
+}
+
+public class CleanerLibrary
+{
+    public Books Books { get; set; }
+}
+
+public class WorstLibrary
+{
+    public List<string> Books { get; set; }
+}
+
---
+++ DotNet/MKasperczyk.Learning.CleanCodeTips/MKasperczyk.Learning.InterestingPackages/Throw/ThrowExamples.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Throw;
+
+namespace MKasperczyk.Learning.InterestingPackages.Throw
+{
+    public class ThrowExamples
+    {
+        public void Main()
+        {
+            string name = "Michal";
+
+            // longer:
+            if (string.Equals(name, "Michal"))
+                throw new ArgumentException("String should not be equal to 'Michal'");
+            // shorter:
+            name.Throw().IfEquals("Michal"); // Same thing, the same message body
+
+
+
+            // string
+            name.Throw("CustomMessage").IfEquals("Michal");
+            name.Throw(() => new Exception()).IfEquals("Michal");
+
+            name.Throw(() => new Exception())
+                    .IfEquals("Michal") // many conditions
+                    .IfLongerThan(2)
+                    .IfContains("test")
+                .Throw("AnotherException") // throw secound exception for other conditions
+                    .IfNotStartsWith("W");
+
+
+            // If Null
+            name.ThrowIfNull();
+
+
+
+            // Collection 
+            var collection = new List<string>();
+            collection.Throw()
+                .IfEmpty()
+                .IfContains("test");
+
+            // Many others like https statuses, files itd. itp. 
+
+            // Extensions
+            Student student = new () { Id = 1, Name = "Johny", Degree = 1 };
+            student.Throw()
+                .IfHasLowerDegreeThen2();
+        }
+    }
+
+    public static class ValidatableExtensions
+    {
+        public static ref readonly Validatable<Student> IfHasLowerDegreeThen2(this in Validatable<Student> validatable)
+        {
+            if(validatable.Value.Degree <= 2)
+            {
+                throw new Exception("Degree lower then 2"); // Of course better to throw own exception, not this one.
+            }
+
+            return ref validatable;
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Degree { get; set; }
+    }
+}
